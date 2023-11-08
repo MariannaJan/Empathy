0e85670e1d9a99041d2a92de28977182314d7a68
Marianna Janczak <janczak.marianna@gmail.com>
2023-11-08 09:07:55 +0100

    test setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Empathy app is running!');
+    expect(compiled.querySelector('[data-test-id="game-entry"]')?.textContent).toContain('Get the game');
   });
 });
