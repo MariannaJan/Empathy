0eaf59378a0b6d0e7faca1419eb987b92885428f
Marianna Janczak <janczak.marianna@gmail.com>
2023-11-08 14:55:52 +0100

    pouchDb

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import PouchDB from 'pouchdb';
 
 @Component({
   selector: 'app-game',
@@ -8,4 +9,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {}
+export class GameComponent {
+  private db: PouchDB.Database;
+
+  constructor() {
+    this.db = new PouchDB('firstDb');
+    this.db.info().then(function (info) {
+      console.log(info);
+    });
+    this.db
+      .put({
+        _id: 'lol',
+        name: 'iza',
+      })
+      .catch(err => {
+        if (err.name === 'conflict') {
+          console.log('conflict');
+        } else {
+          throw err;
+        }
+      });
+  }
+}
